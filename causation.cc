d3808c917b6ad5ab68a1c8ffc5dbc06e878c5691
Murray <murray.t.miron@gmail.com>
2016-08-07 21:26:09 -0400

    Implement buggy ring buffers.

--- causation.cc
+++ causation.cc
@@ -11,21 +11,12 @@
 #include <time.h>
 #include <math.h>
 
-#define SHOW_IMAGES 0
+#define SHOW_IMAGES 1
+
+#define RING_SIZE 5
 #define REPOSITION_CURSOR_LAST_LINE std::cout << '\x08' << '\x0D'
 
-/*
-"0: CAP_OPENNI_DEPTH_MAP 	Depth values in mm (CV_16UC1)",
-"1: CAP_OPENNI_POINT_CLOUD_MAP 	XYZ in meters (CV_32FC3)",
-"2: CAP_OPENNI_DISPARITY_MAP 	Disparity in pixels (CV_8UC1)",
-"3: CAP_OPENNI_DISPARITY_MAP_32F 	Disparity in pixels (CV_32FC1)",
-"4: CAP_OPENNI_VALID_DEPTH_MASK 	CV_8UC1",
-"5: CAP_OPENNI_BGR_IMAGE 	Data given from RGB image generator",
-"6: CAP_OPENNI_GRAY_IMAGE 	Data given from RGB image generator",
-"7: CAP_OPENNI_IR_IMAGE 	Data given from IR image generator",
-*/
-
-static const char* channel_map[] = {
+static const char* visual_channel_map[] = {
 	"CAP_OPENNI_DEPTH_MAP        ",
 	"CAP_OPENNI_POINT_CLOUD_MAP  ",
 	"CAP_OPENNI_DISPARITY_MAP    ",
@@ -33,7 +24,8 @@ static const char* channel_map[] = {
 	"CAP_OPENNI_VALID_DEPTH_MASK ",
 	"CAP_OPENNI_BGR_IMAGE        ",
 	"CAP_OPENNI_GRAY_IMAGE       ",
-	"CAP_OPENNI_IR_IMAGE         ",
+//	"CAP_OPENNI_IR_IMAGE         ",
+	NULL,
 };
 
 /*
@@ -50,46 +42,79 @@ static const char* channel_map[] = {
  */
 bool visual_iter(struct timespec &event)
 {
+	static const float DELTA_THRESHOLD = 0.10;
+
 	static cv::VideoCapture cam(cv::CAP_OPENNI);
-	static cv::Mat framebuf;
 	static std::vector<cv::Mat> vidFrame;
-	static std::vector<float> old_norm, cur_norm, delta;
+
+	// ring buffers for tracking changes over multiple calls
+	static std::vector<float> norm[RING_SIZE], delta[RING_SIZE];
+	static int ringidx = 0;
+
+	// Index of the last iteration's ring buffer ptr
+	int lringidx = (ringidx == 0 ? RING_SIZE - 1 : ringidx - 1);
+
 	bool event_took_place = false;
 
 	if (!cam.isOpened())
 		throw "failed to open camera";
 
 	cam.grab();
-	for (int i = 0; i < 7; i++)
+	for (int i = 0; visual_channel_map[i] != NULL; i++)
 	{
+		// Make sure the framebuffer has allocated a Mat for each channel
 		if (vidFrame.size() < i+1) {
 			vidFrame.push_back(cv::Mat());
-			old_norm.push_back(0.0);
-			cur_norm.push_back(0.0);
-			delta.push_back(0.0);
 		}
-		cam.retrieve(framebuf, i);
-		cur_norm[i] = cv::norm(framebuf);
-		if (SHOW_IMAGES) {
-			framebuf.copyTo(vidFrame[i]);
-			imshow(channel_map[i], vidFrame[i]);
-		}
-	}
-	if (SHOW_IMAGES)
-		cv::waitKey(10);
 
-	for (int i = 0; i < vidFrame.size(); i++) {
-		delta[i] = abs(cur_norm[i] - old_norm[i]);
-		std::cout << channel_map[i] << "\t" << cur_norm[i] << " \t " << delta[i] << std::endl;
+		// Make sure the ring buffer ptrs have allocated a slot for each channel
+		if (norm[ringidx].size() < vidFrame.size()) {
+			norm[ringidx].push_back(0.0);
+			delta[ringidx].push_back(0.0);
+		}
+
+		// Retrieve the frame from channel i and save its matrix norm
+		cam.retrieve(vidFrame[i], i);
+		norm[ringidx][i] = cv::norm(vidFrame[i]);
 	}
 
+	// Print out the values and calculate the deltas of the norms between last call and this one
 	for (int i = 0; i < vidFrame.size(); i++) {
-		old_norm[i] = cur_norm[i];
-		if ( (cur_norm[i] * 0.10) <= delta[i] )
+		if (norm[lringidx].size() > 0)
+			delta[ringidx][i] = abs(norm[ringidx][i] - norm[lringidx][i]);
+		std::cout << visual_channel_map[i] << "\t" << norm[ringidx][i] << " \t " << delta[ringidx][i] << std::endl;
+	}
+
+	// Check for sufficiently large variations throughout entire ring buffer
+	for (int i = 0; i < vidFrame.size(); i++) {
+		/*
+		// Check threshold against weighted value of each tracked delta and this iteration's delta
+		for (int j = 0; j < RING_SIZE && delta[j].size() > 0; j++)
+			if ( (norm[ringidx][i] * DELTA_THRESHOLD) <= ((delta[j][i] + delta[ringidx][i]) / 1.5) ) {
+				event_took_place = !clock_gettime(CLOCK_REALTIME, &event);
+				if (SHOW_IMAGES)
+					imshow(visual_channel_map[i], vidFrame[i]);
+			}
+		*/
+		// check threshold against this iteration's delta alone
+		if ( (norm[ringidx][i] * DELTA_THRESHOLD) <= delta[ringidx][i] ) {
 			event_took_place = !clock_gettime(CLOCK_REALTIME, &event);
+			if (SHOW_IMAGES)
+				imshow(visual_channel_map[i], vidFrame[i]);
+		}
 		REPOSITION_CURSOR_LAST_LINE;
 	}
 
+	// HighGUI needs this to repaint the image in the window
+	if (SHOW_IMAGES)
+		cv::waitKey(1);
+
+	if (++ringidx >= RING_SIZE)
+		ringidx = 0;
+
+//	if (event_took_place)
+//		for (int i = 0; i < RING_SIZE && delta[i].size() > 0; i++)
+//			delta[i].clear();
 	return event_took_place;
 }
 
@@ -112,7 +137,7 @@ int main(int argc, char **argv)
 			REPOSITION_CURSOR_LAST_LINE;
 			std::cout << "visual event took place at " << event_times[0].tv_sec << "." << event_times[0].tv_nsec << std::endl;
 		}
-		usleep(40000);
+//		usleep(40000);
 	}
 	return 0;
 }
