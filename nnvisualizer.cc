242e4173ee64ba51be8480c6d9287bedb94ffa05
Murray <murray.t.miron@gmail.com>
2016-08-19 10:09:50 -0400

    Use Vec3b to fix incorrect usage of pixel values (was CV_8UC1)

--- nnvisualizer.cc
+++ nnvisualizer.cc
@@ -1,25 +1,33 @@
 #include "temporalnn.hh"
 #include <opencv2/core.hpp>
 #include <opencv2/highgui.hpp>
+#include <iostream>
 
 //#define MAX(x, y) (x > y ? x : y)
 //#define MIN(x, y) (x < y ? x : y)
 
 using namespace TemporalNet;
 using namespace cv;
+using namespace std;
 
 Mat createImageFromNet(NeuralNet &net, int height, int width)
 {
-	Mat image(width, height, CV_8UC1);
-	unsigned char color = 10;
+	Mat image(width, height, CV_8UC3, Vec3b(0,0,0));
+	Vec3b color(1,0,1);
 	int y = net.neurons.size();
 	int x = net.neurons[0].size();
-	int x_scale = (x % height);
-	int y_scale = (y % width);
+	int x_pos = 0;
+	int y_pos = 0;
+	int num = 0;
 
-	for (int x = 0; x < net.neurons.size(); x++)
-		for (int y = 0; y < net.neurons[x].size(); y++)
-			image.at<unsigned char>(x + x_scale, y + y_scale) = color;
+	if (x < width)
+		x_pos = (width / x);
+	if (y < height)
+		y_pos = (height / y);
+
+	for (int i = 0; i < x; i++)
+		for (int j = 0; j < y; j++)
+			image.at<Vec3b>(i * x_pos, j * y_pos) = color * MAX(1,net.neurons[i][j].numberOfConnections());
 
 	return image;
 }
