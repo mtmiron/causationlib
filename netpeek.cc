e01b2ff5f33009f09dd896da1b9cd56a6221c042
Murray <murray.t.miron@gmail.com>
2016-08-19 10:12:59 -0400

    Make default neural net dimensions play nicely with the rendering window size

--- netpeek.cc
+++ netpeek.cc
@@ -7,11 +7,12 @@ using namespace cv;
 
 int main(int argc, char **argv)
 {
-	NeuralNet net;
+	NeuralNet net(512,384);
 	Mat image;
+	namedWindow("Neural Net", CV_WINDOW_AUTOSIZE);
 
 	image = createImageFromNet(net);
-	imshow("neural net", image);
+	imshow("Neural Net", image);
 
 	while (unsigned char key = waitKey(0))
 		if (key == 27)
