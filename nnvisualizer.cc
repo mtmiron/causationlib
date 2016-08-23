96053f66d9296e5135f51f61f023fd59a2f2a2c6
Murray <murray.t.miron@gmail.com>
2016-08-22 20:59:20 -0400

    Update connection density coloring for expected use cases.

--- nnvisualizer.cc
+++ nnvisualizer.cc
@@ -10,7 +10,7 @@ using namespace std;
 Mat createImageFromNet(NeuralNet &net, int height, int width)
 {
 	Mat image(width, height, CV_8UC3, Vec3b(0,0,0));
-	Vec3b color(1,0,1);
+	Vec3b color(10,0,10);
 	int x = net.neurons.size();
 	int y = net.neurons[0].size();
 	float x_pos = 0;
@@ -21,7 +21,7 @@ Mat createImageFromNet(NeuralNet &net, int height, int width)
 
 	for (int i = 0; i < x; i++)
 		for (int j = 0; j < y; j++)
-			image.at<Vec3b>( MIN(round(i * x_pos), width - 1), MIN(round(j * y_pos), height - 1) ) = color * MAX(1,net.neurons[i][j].numberOfConnections());
+			image.at<Vec3b>( MIN(round(i * x_pos), width - 1), MIN(round(j * y_pos), height - 1) ) = color * net.neurons[i][j].numberOfConnections();
 
 	return image;
 }
