5cc48802e4e05c11e6b4d2dbd0f1de4137c8d7eb
Murray <murray.t.miron@gmail.com>
2016-08-24 14:43:01 -0400

    Add command line options for several values

--- netpeek.cc
+++ netpeek.cc
@@ -1,31 +1,78 @@
 #include <unistd.h>
-#include <iostream>
+#include <stdio.h>
 #include "temporalnn.hh"
 #include <opencv2/highgui.hpp>
 
+#define DEFAULT_STEP_SIZE 2
+#define DEFAULT_NET_HEIGHT 480
+#define DEFAULT_NET_WIDTH 320
 
 using namespace TemporalNet;
 using namespace std;
 using namespace cv;
+
+
+static struct options {
+	int stepsize;
+	int height;
+	int width;
+} options;
+
+
+void print_help(char **argv)
+{
+	printf("Usage: %s [opts]\n\n\tOptions are:\n\
+\t\t-h\tThis help\n\
+\t\t-s\tStep size for neuron input loop\n\
+\t\t-x\tWidth of neural net\n\
+\t\t-y\tHeight of neural net\n\
+\n", *argv);
+	exit(0);
+}
+
+struct options parse_args(int argc, char **argv)
+{
+	options = { DEFAULT_STEP_SIZE, DEFAULT_NET_HEIGHT, DEFAULT_NET_WIDTH };
+	int c = 0;
+
+	while ((c = getopt(argc, argv, "x:y:hs:")) != -1)
+	{
+		switch (c) {
+		case 's':
+			options.stepsize = atoi(optarg);
+			break;
+		case 'h':
+			print_help(argv);
+			break;
+		case 'x':
+			options.width = atoi(optarg);
+			break;
+		case 'y':
+			options.height = atoi(optarg);
+			break;
+		}
+	}
+	return options;
+}
 
 int main(int argc, char **argv)
 {
 	NeuralNet *net;
 	Mat densities_image, activity_image;
 
-	if (argc < 3)
-		net = new NeuralNet(600,800);
-	else
-		net = new NeuralNet(atoi(argv[1]), atoi(argv[2]));
+	options = parse_args(argc, argv);
+	fprintf(stdout, "Neuron step size: %d  Net height: %d  Net width: %d\n",
+			options.stepsize, options.height, options.width);
 
-
+	net = new NeuralNet(options.width, options.height);
 	net->setupNeurons();
+
 	namedWindow("current activity", CV_WINDOW_AUTOSIZE);
 	namedWindow("connection densities", CV_WINDOW_AUTOSIZE);
 	for (uchar key = 0; key != 27; key = waitKey(1))
 	{
-		for (int i = 100; i < 110; i++)
-			for (int j = 100; j < 110; j++)
+		for (int i = 10; i < net->neurons.size() - 10; i += options.stepsize)
+			for (int j = 10; j < net->neurons[i].size() - 10; j += options.stepsize)
 				net->neurons[i][j].input();
 
 		densities_image = net->createConnectionDensityImage(800, 600);
