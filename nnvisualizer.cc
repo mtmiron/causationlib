61345663778649ac151e3efe6b7400d33045d5d0
Murray <murray.t.miron@gmail.com>
2016-08-27 21:14:54 -0400

    Use TortoiseTime, not struct timespec

--- nnvisualizer.cc
+++ nnvisualizer.cc
@@ -31,7 +31,7 @@ Mat NeuralNet::createConnectionDensityImage(int height, int width)
 	return image;
 }
 
-Mat NeuralNet::createCurrentActivityImage(int height, int width, struct timespec &at_time)
+Mat NeuralNet::createCurrentActivityImage(int height, int width, struct TortoiseTime &at_time)
 {
 	Mat image(width, height, CV_8UC3, Vec3b(0,0,0));
 	Vec3b basecolor(0,0,255);
@@ -52,7 +52,7 @@ Mat NeuralNet::createCurrentActivityImage(int height, int width, struct timespec
 		{
 			if (this->neurons[i][j].firetime.tv_sec == 0)
 				continue;
-			diff = (uint)(timespec_to_ms(timespec_minus(at_time, this->neurons[i][j].firetime)));
+			diff = (uint)((at_time - this->neurons[i][j].firetime).tv_nsec / pow(10,6));
 			color = basecolor / MAX(1, diff / this->neurons[i][j].excited_time);
 			image.at<Vec3b>( MIN(round(i * x_pos), width - 1), MIN(round(j * y_pos), height - 1) ) = color;
 		}
