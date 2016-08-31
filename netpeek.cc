a2fcaf5f0c3abdf077c7232101ce207089bfb00a
Murray <murray.t.miron@gmail.com>
2016-08-30 22:55:40 -0400

    Add option to randomize step size

--- netpeek.cc
+++ netpeek.cc
@@ -22,10 +22,12 @@ struct options {
 	int loop_time;
 	int input_strength;
 	uint layers;
+	bool random_step;
 	bool no_density_image;
 	bool no_activity_image;
 };
 
+struct options opts;
 
 void print_help(char *argv)
 {
@@ -33,9 +35,10 @@ void print_help(char *argv)
 		"\t\t-h\tThis help\n"
 		"\t\t-d\tDon't draw neural connection density image\n"
 		"\t\t-a\tDon't draw neural activity image\n"
+		"\t\t-r\tRandom step size for neuron\n"
+		"\t\t-s ARG\tStep size for neuron input loop\n"
 		"\t\t-x ARG\tWidth of neural net\n"
 		"\t\t-y ARG\tHeight of neural net\n"
-		"\t\t-s ARG\tStep size for neuron input loop\n"
 		"\t\t-i ARG\tSet the input strength to each neuron\n"
 		"\t\t-t ARG\tSet the wait time between wave-input iterations\n"
 		"\t\t-l ARG\tNumber of neural nets\n"
@@ -46,10 +49,10 @@ void print_help(char *argv)
 struct options parse_args(int argc, char **argv)
 {
 	struct options opts = { DEFAULT_STEP_SIZE, DEFAULT_NET_HEIGHT, DEFAULT_NET_WIDTH,
-								DEFAULT_LOOP_TIME, DEFAULT_INPUT_STRENGTH, DEFAULT_LAYERS };
+								DEFAULT_LOOP_TIME, DEFAULT_INPUT_STRENGTH, DEFAULT_LAYERS, false };
 	int c = 0;
 
-	while ((c = getopt(argc, argv, "x:y:hs:dai:l:t:")) != -1)
+	while ((c = getopt(argc, argv, "x:y:hs:dai:l:t:r")) != -1)
 	{
 		switch (c) {
 		case 's':
@@ -76,6 +79,9 @@ struct options parse_args(int argc, char **argv)
 		case 'l':
 			opts.layers = atoi(optarg);
 			break;
+		case 'r':
+			opts.random_step = true;
+			break;
 		case 'h':
 		case '?':
 		default:
@@ -85,15 +91,32 @@ struct options parse_args(int argc, char **argv)
 	return opts;
 }
 
+void interval_step(vector<NeuralNet *> nets, TortoiseTime &at_time)
+{
+	NeuralNet *net = nets[0];
+
+	for (uint i = 10; i < net->neurons.size() - 10; i += opts.stepsize)
+		for (uint j = 10; j < net->neurons[i].size() - 10; j += opts.stepsize)
+			net->neurons[i][j].input(opts.input_strength, at_time);
+}
+
+void random_step(vector<NeuralNet *> nets, TortoiseTime &at_time)
+{
+	NeuralNet *net = nets[0];
+
+	for (uint i = (rand() % opts.stepsize); i < net->neurons.size() - 10; i += (rand() % opts.stepsize))
+		for (uint j = (rand() % opts.stepsize); j < net->neurons[i].size() - 10; j += (rand() % opts.stepsize))
+			net->neurons[i][j].input(opts.input_strength, at_time);
+}
+
 int main(int argc, char **argv)
 {
 	vector<NeuralNet *> nets;
-	NeuralNet *net;
 	Mat densities_image, activity_image;
 	struct TortoiseTime at_time;
-	struct options opts = parse_args(argc, argv);
 	char windowname[256] = { 0 };
 
+	opts = parse_args(argc, argv);
 	fprintf(stdout, "Images are updated every %dms; press ESC to exit\n"
 			"Neuron input strength: %dmV  Neuron step size: %d  Net height: %d  Net width: %d  "
 			"Firing wave loop time: %dms  Net layers: %d\n",
@@ -105,31 +128,33 @@ int main(int argc, char **argv)
 	for (uint i = 0; i < opts.layers - 1; i++)
 		nets[i]->connectTo(nets[i+1]);
 
-	net = nets[0];
+	srand((int)at_time.tv_sec);
 	for (uchar key = 0; key != 27; key = waitKey(opts.loop_time))
 	{
 		clock_gettime(CLOCK_REALTIME, &at_time);
+		if (opts.random_step)
+			random_step(nets, at_time);
+		else
+			interval_step(nets, at_time);
 
-		for (uint i = 10; i < net->neurons.size() - 10; i += opts.stepsize)
-			for (uint j = 10; j < net->neurons[i].size() - 10; j += opts.stepsize)
-				net->neurons[i][j].input(opts.input_strength, at_time);
 #ifdef WITH_TORTOISELIB
-		while (BrainCell::event_queue.applyNext() != -1)
-			;
+		while (BrainCell::event_queue.nextIsEarlierThen(at_time) == true)
+			BrainCell::event_queue.applyNext();
 #endif
+
 		for (uint n = 0; n < opts.layers; n++)
 		{
-			 sprintf(windowname, "layer #%d: firing neurons", n);
-			 if (!opts.no_activity_image) {
-				  activity_image = nets[n]->createCurrentActivityImage(480, 360, at_time);
-				  imshow(windowname, activity_image);
-			 }
+			sprintf(windowname, "layer #%d: firing neurons", n);
+			if (!opts.no_activity_image) {
+				activity_image = nets[n]->createCurrentActivityImage(480, 360, at_time);
+				imshow(windowname, activity_image);
+			}
 
-			 sprintf(windowname, "layer #%d: connection densities", n);
-			 if (!opts.no_density_image) {
-				  densities_image = nets[n]->createConnectionDensityImage(480, 360);
-				  imshow(windowname, densities_image);
-			 }
+			sprintf(windowname, "layer #%d: connection densities", n);
+			if (!opts.no_density_image) {
+				densities_image = nets[n]->createConnectionDensityImage(480, 360);
+				imshow(windowname, densities_image);
+			}
 		}
 	}
 
