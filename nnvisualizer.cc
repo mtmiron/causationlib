af601a9e509508e942fa4036675f8208755ff639
Murray <murray.t.miron@gmail.com>
2016-08-31 21:54:52 -0400

    Add specifying the fade time for action potentials (in seconds)

--- nnvisualizer.cc
+++ nnvisualizer.cc
@@ -1,8 +1,21 @@
 #include "temporalnn.hh"
 #include <opencv2/core.hpp>
+#include <iostream>
 
 using namespace cv;
 using namespace std;
+
+#if 0
+#define DEBUG_OUTPUT \
+			cout << "(" << i << "," << j << ")" " --> "; \
+			cout << "(" << MIN(round(i * x_pos), width - 1) << "," << MIN(round(j * y_pos), height - 1) << ")\t== {" << time_delta << ": " << diff << "} " << color << endl;
+#else
+#define DEBUG_OUTPUT
+#endif
+
+#define PIXEL_X(neuron_x, x_scale) MIN(round(neuron_x * x_scale), width - 1)
+#define PIXEL_Y(neuron_y, y_scale) MIN(round(neuron_y * y_scale), height - 1)
+
 
 Mat NeuralNet::createConnectionDensityImage(int height, int width)
 {
@@ -29,16 +42,19 @@ Mat NeuralNet::createConnectionDensityImage(int height, int width)
 	return image;
 }
 
-Mat NeuralNet::createCurrentActivityImage(int height, int width, struct TortoiseTime &at_time)
+Mat NeuralNet::createCurrentActivityImage(int height, int width, TortoiseTime at_time, int fade_time)
 {
 	Mat image(width, height, CV_8UC3, Vec3b(0,0,0));
-	Vec3b basecolor(0,0,255);
+	Vec3b pixel(0,0,255);
 	Vec3b color(0,0,0);
 	int x = this->neurons.size();
 	int y = this->neurons[0].size();
 	float x_pos = 0;
 	float y_pos = 0;
-	int diff = 0;
+	long long unsigned int diff = 0;
+	long long unsigned int max = pow(10,9) * fade_time;
+	TortoiseTime time_delta;
+	struct timespec oldest = { fade_time, 0 };
 
 	x_pos = ((float)width / x);
 	y_pos = ((float)height / y);
@@ -46,11 +62,17 @@ Mat NeuralNet::createCurrentActivityImage(int height, int width, struct Tortoise
 	for (int i = 0; i < x; i++)
 		for (int j = 0; j < y; j++)
 		{
-			if (this->neurons[i][j].firetime == 0 || this->neurons[i][j].firetime > at_time)
+			time_delta = at_time - this->neurons[i][j].firetime;
+			if (time_delta > oldest || time_delta <= 0)
 				continue;
-			diff = (at_time - this->neurons[i][j].firetime).tv_nsec / pow(10,6);
-			color = basecolor / (diff / this->neurons[i][j].excited_time);
-			image.at<Vec3b>( MIN(round(i * x_pos), width - 1), MIN(round(j * y_pos), height - 1) ) = color;
+
+			diff = (time_delta.tv_sec * pow(10,9) + time_delta.tv_nsec) / (max / 255);
+			color = Vec3b(0,0,255 - diff);
+			pixel = image.at<Vec3b>( PIXEL_X(i, x_pos), PIXEL_Y(j, y_pos) );
+			if (pixel[2] == 0)
+				image.at<Vec3b>( PIXEL_X(i, x_pos), PIXEL_Y(j, y_pos) ) = color;
+
+			DEBUG_OUTPUT;
 		}
 
 	return image;
