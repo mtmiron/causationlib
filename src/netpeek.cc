dfb6015df78dbd911b78f31c0a6b2dd6d6fbbddb
Murray <murray.t.miron@gmail.com>
2016-09-05 18:02:30 -0400

    Only use one default (from temporalnn.cc)

--- src/netpeek.cc
+++ src/netpeek.cc
@@ -28,10 +28,11 @@ struct options {
 	int loop_time = DEFAULT_LOOP_TIME;
 	int input_strength = DEFAULT_INPUT_STRENGTH;
 	uint layers = DEFAULT_LAYERS;
-	int fade_time = DEFAULT_FADE_TIME;
-	int propagation_time = DEFAULT_PROPAGATION_TIME;
-	int max_dendrite_bulge = DEFAULT_MAX_DENDRITE_BULGE;
-	int excited_time = DEFAULT_EXCITED_TIME;
+	float fade_time = DEFAULT_FADE_TIME;
+	int propagation_time = 0;
+	int max_dendrite_bulge = 0;
+	int excited_time = 0;
+	int refractory_time = 0;
 	bool random_step = DEFAULT_RANDOM_FIRING;
 	bool input_enabled = true;
 	bool freeze_connections = false;
@@ -51,9 +52,9 @@ void print_help(char *argv)
 		"\t\t-h\tThis help\n"
 		"\t\t-d\tDon't draw neural connection density image\n"
 		"\t\t-a\tDon't draw neural activity image\n"
-		"\t\t-r\tRandom step size for neuron input loop\n"
+		"\t\t-R\tDon't randomize step size for neuron input loop\n"
 		"\t\t-F\tDon't self assemble neurons (freeze state)\n"
-		"\t\t-p\tPropagation time (time from neuron firing to hitting next neuron)\n"
+		"\t\t-p ARG\tPropagation time (time from neuron firing to hitting next neuron)\n"
 		"\t\t-f ARG\tFade time for activity image\n"
 		"\t\t-s ARG\tStep size for neuron input loop\n"
 		"\t\t-x ARG\tWidth of neural net\n"
@@ -63,6 +64,7 @@ void print_help(char *argv)
 		"\t\t-l ARG\tNumber of neural nets\n"
 		"\t\t-m ARG\tMax dendritic bulge (directly related to max connections)\n"
 		"\t\t-e ARG\tSet excited_time ('concurrent' input within this time will sum)\n"
+		"\t\t-r ARG\tSet neuron refractory time (time period until they'll fire again)\n"
 		"\n", argv);
 	exit(0);
 }
@@ -70,9 +72,10 @@ void print_help(char *argv)
 
 void print_status()
 {
-	fprintf(stdout, "Input strength: %dmV  Step size: %d  Net height: %d  Net width: %d  "
-			"Firing wave loop time: %dms  Net layers: %d  Fade time: %d  Propagation time: %dms  "
+	fprintf(stdout, "\nInput: %dmV  Step size: %d  Net height: %d  Net width: %d\n"
+			"Firing wave: %dms  Net layers: %d  Fade time: %f  Propagation time: %dms  Refractory time: %dms\n"
 			"Max dendrite bulge: %d  Excited duration: %dms\n\n"
+
 			"Images are updated once per firing wave (%dms); red means firing, blue means sub-action-potential stimulation, green is firing triggered by concurrent weak stimulation\n\n"
 
 			"Keymap:\n"
@@ -87,14 +90,13 @@ void print_status()
 			"'f' - freeze connections\n"
 			"'-' - decrease step size\n"
 			"'+' - increase step size\n\n"
-
-			"Mouse:\n"
-			"LBUTTON - fire neurons at mouse (x,y) position\n"
-			"RBUTTON - call NeuralNet's handleDendriticBulge() function\n",
+			"Mouse LButton - fire neurons at mouse (x,y) position\n"
+			"Mouse RButton - call NeuralNet's growDendrite() function\n",
 
 			opts.input_strength, opts.stepsize, opts.height, opts.width,
 			opts.loop_time, opts.layers, opts.fade_time, opts.propagation_time,
-			opts.max_dendrite_bulge, opts.excited_time, opts.loop_time);
+			opts.refractory_time, opts.max_dendrite_bulge, opts.excited_time,
+			opts.loop_time);
 }
 
 
@@ -102,7 +104,7 @@ struct options parse_args(int argc, char **argv)
 {
 	int c = 0;
 
-	while ((c = getopt(argc, argv, "x:y:hs:dai:l:t:rFf:p:m:e:")) != -1)
+	while ((c = getopt(argc, argv, "x:y:hs:dai:l:t:Rr:Ff:p:m:e:")) != -1)
 	{
 		switch (c) {
 		case 's':
@@ -129,14 +131,17 @@ struct options parse_args(int argc, char **argv)
 		case 'l':
 			opts.layers = atoi(optarg);
 			break;
+		case 'R':
+			opts.random_step = !opts.random_step;
+			break;
 		case 'r':
-			opts.random_step = true;
+			opts.refractory_time = atoi(optarg);
 			break;
 		case 'p':
 			opts.propagation_time = atoi(optarg);
 			break;
 		case 'f':
-			opts.fade_time = atoi(optarg);
+			opts.fade_time = atof(optarg);
 			break;
 		case 'F':
 			opts.freeze_connections = true;
@@ -193,12 +198,14 @@ void handle_keypress(uchar key)
 		break;
 	case '-':
 	case '_':
+	case 171:
 		if (opts.stepsize > 1)
 			opts.stepsize--;
 		cout << "Step size: " << opts.stepsize << endl;
 		break;
 	case '+':
 	case '=':
+	case 173:
 		cout << "Step size: " << ++opts.stepsize << endl;
 		break;
 	}
@@ -216,7 +223,7 @@ void activity_window_mouse_callback(int event, int x, int y, int flags, void *us
 	case EVENT_RBUTTONDOWN: rbutton:
 		rbutton_down = true;
 		neuron = &net->getFromWindowPosition(x, y, DEFAULT_NET_WIDTH, DEFAULT_NET_HEIGHT);
-		net->handleDendriticBulge(neuron, neuron->numberOfConnections() + 1);
+		net->growDendrite(neuron, neuron->numberOfConnections() + 1);
 		break;
 	case EVENT_LBUTTONDOWN: lbutton:
 		lbutton_down = true;
@@ -304,7 +311,6 @@ int main(int argc, char **argv)
 	opts = parse_args(argc, argv);
 	clock_gettime(CLOCK_REALTIME, &at_time);
 	srandom((int)at_time.tv_sec);
-	print_status();
 
 	for (uint i = 0; i < opts.layers; i++)
 	{
@@ -312,18 +318,32 @@ int main(int argc, char **argv)
 		for (int x = 0; x < opts.width; x++)
 			for (int y = 0; y < opts.height; y++)
 			{
-				nets[i]->neurons[x][y].setPropagationTime(opts.propagation_time);
-				nets[i]->neurons[x][y].setMaxDendriteConnections(opts.max_dendrite_bulge);
-				nets[i]->neurons[x][y].setExcitedTime(opts.excited_time);
+				if (opts.propagation_time)
+					nets[i]->neurons[x][y].setPropagationTime(opts.propagation_time);
+				else
+					opts.propagation_time = nets[i]->neurons[x][y].propagation_time;
+				if (opts.max_dendrite_bulge)
+					nets[i]->neurons[x][y].setMaxDendriteConnections(opts.max_dendrite_bulge);
+				else
+					opts.max_dendrite_bulge = nets[i]->neurons[x][y].max_dendrite_bulge;
+				if (opts.excited_time)
+					nets[i]->neurons[x][y].setExcitedTime(opts.excited_time);
+				else
+					opts.excited_time = nets[i]->neurons[x][y].excited_time;
+				if (opts.refractory_time)
+					nets[i]->neurons[x][y].setRefractoryTime(opts.refractory_time);
+				else
+					opts.refractory_time = nets[i]->neurons[x][y].refractory_time;
 			}
 	}
+	print_status();
 
 	for (uint i = 0; i < opts.layers - 1; i++)
 		nets[i]->connectTo(nets[i+1]);
 
   try {
 	// out of memory buffer, so the catch() has some when it goes out of scope
-	char oom_buf[32 * 1024];
+	volatile char oom_buf[32 * 1024];
 
 	for (uchar key = 0; key != 27; key = waitKey(opts.loop_time))
 	{
